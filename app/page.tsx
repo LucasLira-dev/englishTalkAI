2d75fe860d282b30bc732f8d1e125fb9eb97b871
lucas mendes <lucasmndes.dev@gmail.com>
2025-11-04 19:59:17 -0300

    criando section de apresentação

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { Header } from '../components/Header/header';
 import { Sparkles } from 'lucide-react';
 
@@ -7,7 +8,7 @@ export default function Home() {
     className="min-h-screen bg-background">
       <Header />
       <section
-      className="flex flex-col items-center justify-center py-24">
+      className="flex flex-col items-center justify-center py-24 mt-8 md:mt-12 lg:mt-16">
         <div
          className='flex gap-2 rounded-3xl py-2 px-6 bg-muted-foreground/20 border-ring max-w-sm'>
           <Sparkles className="w-6 h-6 text-chart-5" />
@@ -15,6 +16,35 @@ export default function Home() {
             className="text-accent font-bold">
             Prática poderosa com IA
           </span>
+        </div>
+        <div
+        className='flex flex-col items-center justify-center p-4 gap-5'>
+          <h2 className="text-5xl md:text-6xl font-display font-bold text-balance text-center">
+            Fale Inglês{" "}
+            <span className="bg-linear-to-r from-primary to-accent bg-clip-text text-transparent">
+              Naturalmente
+            </span>
+          </h2>
+          <p
+          className="text-lg md:text-xl text-muted-foreground text-center">
+            Pratique conversação em inglês com um inteligente assistente virtual. Receba feedback instantâneo e melhore sua fluência.
+          </p>
+          <div
+          className="flex flex-col sm:flex-row gap-4 justify-center mb-12">
+            <Button
+            className='bg-primary/80 flex gap-2 hover:bg-primary/70 font-bold cursor-pointer'
+            variant="default"
+            size="lg">
+              <span className="mr-2">🎤</span>
+              Comece a praticar
+            </Button>
+            <Button
+            className='bg-primary-foreground/90 border border-primary hover:bg-accent hover:border-accent hover:text-primary-foreground text-foreground text-center font-normal cursor-pointer w-full'
+            variant="default"
+            size="lg">
+              Saiba Mais
+            </Button>
+          </div>
         </div>
       </section>
     </main>
--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -6,16 +6,16 @@ export const Header = () => {
   return(
     <header>
       <nav className="fixed top-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-md border-b border-border">
-        <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
+        <div className="max-w-6xl mx-auto px-5 py-4 flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-primary to-accent flex items-center justify-center">
+            <div className="w-10 h-10 rounded-lg bg-linear-to-br from-primary to-accent flex items-center justify-center">
               <Sparkles className="w-6 h-6 text-primary-foreground" />
             </div>
             <h1 className="font-display font-bold text-xl text-foreground">EnglishTalkAI</h1>
           </div>
-          <Link href="/auth/login">
-            <Button variant="default" className="bg-primary hover:bg-primary/80 cursor-pointer">
-              Start Learning
+          <Link href="/login">
+            <Button variant="default" className="bg-primary hover:bg-primary/80 cursor-pointer font-bold">
+              Comece
               <ArrowRight className="w-4 h-4 ml-2" />
             </Button>
           </Link>
